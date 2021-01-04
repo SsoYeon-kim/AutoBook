e37f5fca3e8c97faa72735f5aa9234560da44c11
SsoYeon-kim <ksy1442@nate.com>
2021-01-04 16:55:45 +0900

    OCR(tess4J)사용 - 한글,영어

---
+++ OCR/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import net.sourceforge.tess4j.Tesseract;
+
+public class Main {
+
+	static Tesseract instance = Tesseract.getInstance();
+	
+	public static String process(String fileName) {
+		File inputFile = new File(fileName);
+		String result = "";
+		
+		try {
+			result = instance.doOCR(inputFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(process("Book.png"));
+	}
+
+}
